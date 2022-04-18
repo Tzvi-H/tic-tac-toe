d958a2cd754a99c9ad152c11a9f6b71c7761394a
Tzvi-H <16guitar@gmail.com>
2022-04-18 10:05:37 -0500

    basic functionality for marking a tile

--- main.js
+++ main.js
@@ -4,27 +4,54 @@ const Player = function(name) {
   }
 }
 
-const gameBoard = (() => {
-  const x = 'X';
-  const y = 'Y';
-  const board = new Array(9).fill('x')
-
-  return {
-    board
-  }
-})()
+const gameBoard = new Array(9);
 
 const displayController = (() => {
   const boardContainer = document.querySelector('#boardContainer');
+  const interactiveContainer = document.querySelector('#interactiveContainer')
+  const scoresContainer = document.querySelector('#scores')
+  const scores = { computer: 0, user: 0 }
+  let clickedBoardIndex
+  let name;
 
-  const createBoard = () => {
-    gameBoard.board.forEach((position, index) => {
-      boardContainer.children[index].textContent = position
-    })
+  const startGame = e => {
+    name = document.querySelector('input').value
+    scoresContainer.hidden = false;
+    scoresContainer.querySelector('#userName').textContent = name;
+    updateScores()
+    startGameFlow()
+  }
+
+  const handleBoardClick = e => {
+    if (!e.target.dataset.index) return;
+    const index = parseInt(e.target.dataset.index) - 1;
+
+    if (gameBoard[index]) {
+      interactiveContainer.textContent = 'Please choose an available tile'
+      return
+    } else {
+      markBoard(index, 'X')
+    }
+  }
+
+  const markBoard = (index, marker) => {
+    gameBoard[index] = marker;
+    boardContainer.children[index].textContent = marker;
+  }
+
+  const startGameFlow = () => {
+    interactiveContainer.textContent = 'click an available tile'
+    boardContainer.addEventListener('click', handleBoardClick)
+  }
+
+  const updateScores = () => {
+    scoresContainer.querySelector('#computerScore').textContent = scores.computer
+    scoresContainer.querySelector('#userScore').textContent = scores.user
   }
 
   const start = () => {
-    createBoard()
+    document.querySelector('button')
+            .addEventListener('click', startGame)
   }
 
   return {
