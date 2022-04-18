d630f8f08cf9a6b7f8557dd95598d0638da0e9df
Tzvi-H <16guitar@gmail.com>
2022-04-18 09:25:17 -0500

    reapplied styles

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-// You’re going to store the gameboard as an array inside of a Gameboard object, so start there! 
-// Your players are also going to be stored in objects… and you’re probably going to want an object to 
-// control the flow of the game itself.
-// Your main goal here is to have as little global code as possible. Try tucking everything away inside of a 
-// module or factory. Rule of thumb: if you only ever need ONE of something (gameBoard, displayController), 
-// use a module. If you need multiples of something (players!), create them with factories.
-
 const Player = function(name) {
   return {
     name
@@ -24,17 +17,9 @@ const gameBoard = (() => {
 const displayController = (() => {
   const boardContainer = document.querySelector('#boardContainer');
 
-  const createPositionElement = (position, index) => {
-    const element = document.createElement('div')
-    element.textContent = position
-    element.dataset.index = index
-    return element;
-  }
-
   const createBoard = () => {
     gameBoard.board.forEach((position, index) => {
-      const element = createPositionElement(position, index);
-      boardContainer.appendChild(element)
+      boardContainer.children[index].textContent = position
     })
   }
 
