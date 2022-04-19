acdfb593e90f113fdc2016c0146f19411394e41d
Tzvi-H <16guitar@gmail.com>
2022-04-19 11:16:06 -0500

    added playing multiple times, readme

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# tic-tac-toe
-
-Practice with objects creation. 
-Factory functions and modules
+view [here](https://tzvi-h.github.io/tic-tac-toe/)
--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-const Player = function(name) {
-  return {
-    name
-  }
-}
-
-const gameBoard = new Array(9);
+const gameBoard = new Array(9).fill(null);
 
 const displayController = (() => {
   const boardContainer = document.querySelector('#boardContainer');
@@ -22,6 +16,36 @@ const displayController = (() => {
     startGameFlow()
   }
 
+  const isWinner = (board, marker) => {
+    const markers = [...boardContainer.children].map(t => t.textContent)
+    const winningPositionsCombos = [
+      [0,1,2], [3,4,5], [6,7,8],
+      [0,3,6], [1,4,7], [2,5,8],
+      [0,4,8], [2,4,6]
+    ]
+
+    return (
+      winningPositionsCombos.some(combos => {
+        return combos.every(position => {
+          return markers[position] === marker;
+        })
+      })
+    )
+  }
+
+  const boardIsFull = board => {
+    return board.every(tile => tile === 'X' || tile === 'O')
+  }
+
+  const computerGoes = gameBoard => {
+    let randomIndex = Math.floor(Math.random() * 9);
+    while (gameBoard[randomIndex] !== null) {
+      randomIndex = Math.floor(Math.random() * 9)
+    }
+    gameBoard[randomIndex] = 'O'
+    markBoard(randomIndex, 'O')
+  }
+
   const handleBoardClick = e => {
     if (!e.target.dataset.index) return;
     const index = parseInt(e.target.dataset.index) - 1;
@@ -29,9 +53,40 @@ const displayController = (() => {
     if (gameBoard[index]) {
       interactiveContainer.textContent = 'Please choose an available tile'
       return
+    } 
+      
+    markBoard(index, 'X')
+
+    if (isWinner(gameBoard, 'X')) {
+      interactiveContainer.textContent = `${name} wins!`
+      handleFinish()
+    } else if (boardIsFull(gameBoard)) {
+      interactiveContainer.textContent = `no more spaces left!`
+      handleFinish()
     } else {
-      markBoard(index, 'X')
+      computerGoes(gameBoard)
+      if (isWinner(gameBoard, 'O')) {
+        interactiveContainer.textContent = 'computer wins!'
+        handleFinish()
+      } else if (boardIsFull(gameBoard)) {
+        interactiveContainer.textContent = `no more spaces left!`
+        handleFinish()
+      }
     }
+  }
+
+  const handleFinish = () => {
+    boardContainer.removeEventListener('click', handleBoardClick)
+    const button = document.createElement('button')
+    button.textContent = 'restart game'
+    interactiveContainer.insertAdjacentElement('beforeend', button)
+    button.addEventListener('click', restartGame)
+  }
+
+  const restartGame = () => {
+    gameBoard.forEach((_, i) => gameBoard[i] = null);
+    [...boardContainer.children].forEach(t => t.textContent = '')
+    startGameFlow()
   }
 
   const markBoard = (index, marker) => {
